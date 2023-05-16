4fb01e53dbc9c5445c5042f3b090f5177559e7ff
Nicolas Bizé <nicolas.bize@subatech.in2p3.fr>
2023-05-16 19:32:09 +0200

    add dynamic ptcut

--- PWGDQ/Tasks/mumuReader.cxx
+++ PWGDQ/Tasks/mumuReader.cxx
@@ -27,11 +27,16 @@ using namespace o2::framework;
 struct mumuReader {
 
   // histogram created with OutputObj<TH1F>
-  OutputObj<TH1F> chi2MCHMIDLeft{TH1F("chi2MCHMIDLeft", "chi2MCHMIDLeft", 100, 0., 16.)};
-  OutputObj<TH1F> chi2MCHMIDRight{TH1F("chi2MCHMIDRight", "chi2MCHMIDRight", 100, 0., 16.)};
-  OutputObj<TH1F> chi2MCHMIDUp{TH1F("chi2MCHMIDUp", "chi2MCHMIDUp", 100, 0., 16.)};
-  OutputObj<TH1F> chi2MCHMIDDown{TH1F("chi2MCHMIDDown", "chi2MCHMIDDown", 100, 0., 16.)};
+  OutputObj<TH1F> hchi2MCHMIDLeft{TH1F("chi2MCHMIDLeft", "chi2MCHMIDLeft", 100, 0., 16.)};
+  OutputObj<TH1F> hchi2MCHMIDRight{TH1F("chi2MCHMIDRight", "chi2MCHMIDRight", 100, 0., 16.)};
+  OutputObj<TH1F> hchi2MCHMIDUp{TH1F("chi2MCHMIDUp", "chi2MCHMIDUp", 100, 0., 16.)};
+  OutputObj<TH1F> hchi2MCHMIDDown{TH1F("chi2MCHMIDDown", "chi2MCHMIDDown", 100, 0., 16.)};
+  // check MCH-MFT chi2 also
+  OutputObj<TH1F> hChi2MCHMFTLeft{TH1F("chi2MCHMFTLeft", "chi2MCHMFTLeft", 100, 0., 16.)};
+  OutputObj<TH1F> hChi2MCHMFTRight{TH1F("chi2MCHMFTRight", "chi2MCHMFTRight", 100, 0., 16.)};
+
   OutputObj<TH1F> hRapidityAll{TH1F("Rapidity", "Rapidity", 267, 2., 4.)};
+
   OutputObj<TH1F> hRapidityMCHMFT{TH1F("RapidityMCHMFT", "RapidityMCHMFT", 267, 2., 4.)};
   OutputObj<TH1F> hTauz{TH1F("Tauz", "Tauz", 100, -0.01, 0.01)};
   OutputObj<TH1F> hdeltaZ{TH1F("deltaZ", "deltaZ", 1000, -10., 10.)};
@@ -39,6 +44,7 @@ struct mumuReader {
 
   Configurable<bool> usePhi{"usePhi", false, "If true, use phi method"};
   Configurable<bool> useMomentum{"useMomentum", false, "If true, use momentum method"};
+  Configurable<Double_t> ptCut{"ptCut", 0.5, "pt cut, default = 0.5"};
 
   // histogram registry test
   HistogramRegistry registry{
@@ -70,7 +76,7 @@ struct mumuReader {
   void process(aod::DimuonsAll const& dimuons)
   {
     Double_t const PI = ROOT::Math::Pi();
-    
+
     for (auto& dimuon : dimuons) {
       // dimuon variables
       auto rap = -ROOT::Math::log((ROOT::Math::sqrt(dimuon.mass() * dimuon.mass() + dimuon.pt() * dimuon.pt() * ROOT::Math::cosh(dimuon.eta()) * ROOT::Math::cosh(dimuon.eta())) + dimuon.pt() * ROOT::Math::sinh(dimuon.eta())) / (ROOT::Math::sqrt(dimuon.mass() * dimuon.mass() + dimuon.pt() * dimuon.pt())));
@@ -88,73 +94,76 @@ struct mumuReader {
       auto deltaZ = (dimuon.tauz() * pz) / dimuon.mass();
 
       if ((dimuon.eta1() > -4 && dimuon.eta1() < -2.5) && (dimuon.eta2() > -4 && dimuon.eta2() < -2.5)) { // cut on eta in mft acceptance
-        if (dimuon.sign() == 0) {
-          if (dimuon.chi2MatchMCHMFT1() <= 0. && dimuon.chi2MatchMCHMFT2() <= 0.) {
-            if (rap > 2.5 && rap < 4) {
-              // MCH-MID chi2 studies
-              registry.get<TH1>(HIST("JPsiStandard"))->Fill(dimuon.mass());
+        if (dimuon.pt1() >= ptCut && dimuon.pt2() >= ptCut) {
+          if (dimuon.sign() == 0) {
+            if (dimuon.chi2MatchMCHMFT1() <= 0. && dimuon.chi2MatchMCHMFT2() <= 0.) {
+              if (rap > 2.5 && rap < 4) {
+                // MCH-MID chi2 studies
+                registry.get<TH1>(HIST("JPsiStandard"))->Fill(dimuon.mass());
 
-              if (usePhi) {
-                if (dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) {
-                  chi2MCHMIDLeft->Fill(dimuon.chi2MatchMCHMID1());
-                } else if (dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) {
-                  chi2MCHMIDRight->Fill(dimuon.chi2MatchMCHMID1());
-                }
+                if (usePhi) {
+                  if (dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) {
+                    hchi2MCHMIDLeft->Fill(dimuon.chi2MatchMCHMID1());
+                  } else if (dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) {
+                    hchi2MCHMIDRight->Fill(dimuon.chi2MatchMCHMID1());
+                  }
 
-                if ((dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) && (dimuon.phi2() > PI / 2 || dimuon.phi2() < -PI / 2)) {
-                  registry.get<TH1>(HIST("JPsiXNeg"))->Fill(dimuon.mass());
-                }
+                  if ((dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) && (dimuon.phi2() > PI / 2 || dimuon.phi2() < -PI / 2)) {
+                    registry.get<TH1>(HIST("JPsiXNeg"))->Fill(dimuon.mass());
+                  }
 
-                if ((dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) && (dimuon.phi2() < PI / 2 && dimuon.phi2() > -PI / 2)) {
-                  registry.get<TH1>(HIST("JPsiXPos"))->Fill(dimuon.mass());
-                }
+                  if ((dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) && (dimuon.phi2() < PI / 2 && dimuon.phi2() > -PI / 2)) {
+                    registry.get<TH1>(HIST("JPsiXPos"))->Fill(dimuon.mass());
+                  }
 
-                if (dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) {
-                  if (dimuon.phi2() < PI / 2 && dimuon.phi2() > -PI / 2) {
+                  if (dimuon.phi1() > PI / 2 || dimuon.phi1() < -PI / 2) {
+                    if (dimuon.phi2() < PI / 2 && dimuon.phi2() > -PI / 2) {
+                      registry.get<TH1>(HIST("JPsiXNeutral"))->Fill(dimuon.mass());
+                    }
+                  }
+                  if (dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) {
+                    if (dimuon.phi2() > PI / 2 || dimuon.phi2() < -PI / 2) {
+                      registry.get<TH1>(HIST("JPsiXNeutral"))->Fill(dimuon.mass());
+                    }
+                  }
+                } // end if phi method
+
+                if (useMomentum) {
+                  if (px1 < 0. && px2 < 0.) {
+                    hchi2MCHMIDLeft->Fill(dimuon.chi2MatchMCHMID1());
+                  } else if (px1 > 0. && px2 > 0.) {
+                    hchi2MCHMIDRight->Fill(dimuon.chi2MatchMCHMID1());
+                  }
+                  if (py1 < 0. && py2 < 0.) {
+                    hchi2MCHMIDDown->Fill(dimuon.chi2MatchMCHMID1());
+                  } else if (py1 > 0. && py2 > 0.) {
+                    hchi2MCHMIDUp->Fill(dimuon.chi2MatchMCHMID1());
+                  }
+                  if (px1 < 0. && px2 < 0.) {
+                    registry.get<TH1>(HIST("JPsiXNeg"))->Fill(dimuon.mass());
+                  }
+                  if (px1 > 0. && px2 > 0.) {
+                    registry.get<TH1>(HIST("JPsiXPos"))->Fill(dimuon.mass());
+                  }
+                  if ((px1 < 0. && px2 > 0.) || (px1 > 0. && px2 < 0.)) {
                     registry.get<TH1>(HIST("JPsiXNeutral"))->Fill(dimuon.mass());
                   }
-                }
-                if (dimuon.phi1() < PI / 2 && dimuon.phi1() > -PI / 2) {
-                  if (dimuon.phi2() > PI / 2 || dimuon.phi2() < -PI / 2) {
-                    registry.get<TH1>(HIST("JPsiXNeutral"))->Fill(dimuon.mass());
+                  if (py1 < 0. && py2 < 0.) {
+                    registry.get<TH1>(HIST("JPsiYNeg"))->Fill(dimuon.mass());
                   }
-                }
-              } // end if phi method
+                  if (py1 > 0. && py2 > 0.) {
+                    registry.get<TH1>(HIST("JPsiYPos"))->Fill(dimuon.mass());
+                  }
+                  if ((py1 < 0. && py2 > 0.) || (py1 > 0. && py2 < 0.)) {
+                    registry.get<TH1>(HIST("JPsiYNeutral"))->Fill(dimuon.mass());
+                  }
+                } // end of momentum method
 
-              if (useMomentum){
-                if (px1 < 0. && px2 < 0.) {
-                  chi2MCHMIDLeft->Fill(dimuon.chi2MatchMCHMID1());
-                } else if (px1 > 0. && px2 > 0.) {
-                  chi2MCHMIDRight->Fill(dimuon.chi2MatchMCHMID1());
-                }
-                if (py1 < 0. && py2 < 0.) {
-                  chi2MCHMIDDown->Fill(dimuon.chi2MatchMCHMID1());
-                } else if (py1 > 0. && py2 > 0.) {
-                  chi2MCHMIDUp->Fill(dimuon.chi2MatchMCHMID1());
-                }
-                if (px1 < 0. && px2 < 0.){
-                  registry.get<TH1>(HIST("JPsiXNeg"))->Fill(dimuon.mass());
-                }
-                if (px1 > 0. && px2 > 0.){
-                  registry.get<TH1>(HIST("JPsiXPos"))->Fill(dimuon.mass());
-                }
-                if ((px1 < 0. && px2 > 0.) || (px1 > 0. && px2 < 0.)) {
-                  registry.get<TH1>(HIST("JPsiXNeutral"))->Fill(dimuon.mass());
-                }
-                if (py1 < 0. && py2 < 0.){
-                  registry.get<TH1>(HIST("JPsiYNeg"))->Fill(dimuon.mass());
-                }
-                if (py1 > 0. && py2 > 0.){
-                  registry.get<TH1>(HIST("JPsiYPos"))->Fill(dimuon.mass());
-                }
-                if ((py1 < 0. && py2 > 0.) || (py1 > 0. && py2 < 0.)) {
-                  registry.get<TH1>(HIST("JPsiYNeutral"))->Fill(dimuon.mass());
-                }
-              } // end of momentum method
-
-            } // end rapidity cut
+              } // end rapidity cut
+            }
           } // end MCH-MID track selection
-        }   // end OS selection
+        } //end pt cut   
+        // end OS selection
       }     // end eta cut (MUON acceptance)
 
       // MCH-MFT chi2 studies
@@ -167,8 +176,8 @@ struct mumuReader {
           hRapidityMCHMFT->Fill(rap);
           if (dimuon.sign() == 0) {
             if (rap > 2.5 && rap < 3.6) {
+              // TODO check : MCH-MFT matching chi2 distribution for left and right MCH part
               if (dimuon.chi2MatchMCHMFT1() < 45 && dimuon.chi2MatchMCHMFT2() < 45) {
-
                 hTauz->Fill(dimuon.tauz());
               }
               registry.get<TH3>(HIST("massTH3"))->Fill(dimuon.mass(), dimuon.chi2MatchMCHMFT1(), dimuon.chi2MatchMCHMFT2());
